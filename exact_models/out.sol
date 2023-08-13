4e2fb90212f91b59049904feb2a90a78e7a108e8
LucasGabriel-CP <lucasgabrielgarriel@hotmail.com>
2023-08-12 22:10:04 -0300

    Creating instances using Linear Regression

---
+++ exact_models/out.sol
@@ -0,0 +1,15 @@
+# Solution for model probrema
+# Objective value = 410323875.76
+supply_3_port_9 629876
+supply_4_port_9 1253270
+supply_0_tranship_6 243138
+supply_1_tranship_6 130810
+supply_1_tranship_7 223253
+supply_2_tranship_6 143703
+supply_3_tranship_8 125374
+tranship_6_port_9 517651
+tranship_7_port_9 50864
+tranship_8_port_9 125374
+tranship_7_port_10 172389
+X_9_11 2577035
+X_10_11 172389
